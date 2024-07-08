82b79f69cb80de5cc77d5f19126740caabdce10b
ritikp123 <ritikpaswan965@gmail.com>
2024-07-08 09:54:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ if st.button("Seles Prediction "):
     result = clf.predict(np.array([[tv,rd,newspaper]]))
     st.text(result[0])
 
-st.markdown("Developed By Pradeep Ale at NIELIT Daman")
+st.markdown("Developed By RITIK PASWAN at NIELIT Daman")
